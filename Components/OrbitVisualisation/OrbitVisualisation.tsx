c3b897431de68a2ddecd842fefb0e52915a598db
Eldar <hasanovicel@gmail.com>
2022-09-03 22:07:18 -0500

    MORE

--- Components/OrbitVisualisation/OrbitVisualisation.tsx
+++ Components/OrbitVisualisation/OrbitVisualisation.tsx
@@ -1,7 +1,10 @@
 
-import { Canvas } from "@react-three/fiber";
-import { useContext, useRef, useLayoutEffect, LegacyRef } from "react";
+
+import { message, Tooltip } from 'antd'
+import { Canvas, ThreeEvent } from "@react-three/fiber";
+import { useContext, useRef, useLayoutEffect } from "react";
 import * as THREE from 'three'
+
 import AppContext from "../../Contexts/AppContext";
 import { GetOrbitStateVectors, Orbit } from "../../Orbits/Orbit";
 import { MEDIUM_DARK_BLUE } from "../../styles/Colours";
@@ -29,13 +32,10 @@ const GetOrbitOutlinePositionSet = (orbit: Orbit): THREE.Vector3[] => {
     )
 
     const scaledPosition = position.map(x => x / SCALING_FACTOR)
-
     return new THREE.Vector3( scaledPosition[0], scaledPosition[2], scaledPosition[1] )
   })
 
   pointSet.push(pointSet[0])
-
-
   return pointSet
 }
 
@@ -60,12 +60,65 @@ const OrbitLine = (props: { orbit: Orbit }) => {
       <lineBasicMaterial color="white" linewidth={20}/>
     </line>
   )
+}
 
+const PlanetOrbit = (props: { orbit: Orbit }) => {
+
+  const { orbit } = props;
+  const { centralMass, time } = useContext(AppContext)
+
+  const { position } = GetOrbitStateVectors(
+    centralMass.mass,
+    orbit,
+    time
+  )            
+
+  const p = position.map(x => x / SCALING_FACTOR)
+
+  const handleClick = (args: ThreeEvent<PointerEvent>) => {
+
+    console.log({ 
+      msg: `you clicked on ${orbit.name}`,
+      msg2: `looks like it was a ${ args['button'] === 0 ? 'left click' : 'right click' }`
+    }) 
+
+    const isLeftClick = args['button'] === 0;
+    const isRightClick = args['button'] === 2;
+
+    if (isLeftClick) {
+      message.success(`Wow! Left click on ${orbit.name}!`)
+    }
+
+    if (isRightClick) {
+      message.warning(`Uh oh! Right click on ${orbit.name}!`)
+    }
+
+  }
+
+  return (
+    <group>
+      <mesh 
+        position={[ p[0], p[2], p[1] ]} 
+        receiveShadow={true}
+        onPointerUp={handleClick}
+      >
+
+        <sphereGeometry attach="geometry" args={[1, 16, 16]} />
+        <meshPhysicalMaterial color='brown' />
+
+      </mesh>
+
+      <OrbitLine
+        orbit={orbit}
+      />
+
+    </group>
+  )
 }
 
 const OrbitVisualisation = () => {
 
-  const { centralMass, orbitList, time } = useContext(AppContext)
+  const { orbitList } = useContext(AppContext)
 
   return (
     <div style={{
@@ -83,41 +136,21 @@ const OrbitVisualisation = () => {
       >
         <Controls/>
 
-        <ambientLight color={"white"} intensity={0.3} />
+        <ambientLight color={"white"} intensity={0.5} />
 
         <mesh receiveShadow={true}>
-          <boxBufferGeometry args={[1,1,1]} />
-          <meshPhysicalMaterial color='yellow' />
+          <sphereGeometry attach="geometry" args={[1, 16, 16]} />
+          <meshPhysicalMaterial 
+            attach="material"
+            color="yellow"
+            transparent
+            roughness={0.1}
+            metalness={0.1}
+          />
         </mesh>
 
         {
-          orbitList.map((o, i) => {
-            const { orbit } = o;
-
-            const { position } = GetOrbitStateVectors(
-              centralMass.mass,
-              orbit,
-              time
-            )            
-
-            const p = position.map(x => x / SCALING_FACTOR)
-
-            return (
-              <group key={i}>
-                <mesh position={[ p[0], p[2], p[1] ]} receiveShadow={true}>
-
-                  <boxBufferGeometry args={[1,1,1]} />
-                  <meshPhysicalMaterial color='brown' />
-
-                </mesh>
-
-                <OrbitLine
-                  orbit={orbit}
-                />
-
-              </group>
-            )
-          })
+          orbitList.map(o => <PlanetOrbit key={o.orbit.name} orbit={o.orbit} />)
         }
 
       </Canvas>
